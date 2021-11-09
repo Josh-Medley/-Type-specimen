565ad6d64da19042921fbe07a054b4b920e44244
Josh-Medley <89552775+Josh-Medley@users.noreply.github.com>
2021-11-09 15:00:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
+$('#canvas_picker').click(function(event){
+                  var x = event.pageX - $(this).offset().left;
+                  var y = event.pageY - $(this).offset().top;
+                  var img_data = context.getImageData(x,y , 1, 1).data;
+                  var R = img_data[0];
+                  var G = img_data[1];
+                  var B = img_data[2]; 
+                  var rgb = R + ',' + G + ',' + B ;
+                  var hex = rgbToHex(R,G,B);
+                  $('#rgb input').val( rgb );
+                  $('#hex input').val('#' + hex);
+                  $picked.append("<span data-value='#"+hex+"' class='span-value-copy' contenteditable='true' style='background:#"+hex+"'>#"+hex+"</span>");
 
-document.execCommand(aCommandName, aShowDefaultUI, aValueArgument)
+                    function copyTextToClipboard(text) {
+                      var textArea = document.createElement("textarea");
+                      textArea.value = text;
+                      document.body.appendChild(textArea);
+                      textArea.select();
+                      document.execCommand('copy');
+                      document.body.removeChild(textArea);
+                    }
 
-myInputElement.select();
+                    var copyBobBtn = document.querySelector('.span-value-copy');
 
-document.execCommand("Copy");
+                    copyBobBtn.addEventListener('click', function() {
+                      var currentHex =  $(this).data('value');
+                      copyTextToClipboard(currentHex);
+                    });
 
-function copyText(element) {
-
-    var textToCopy = element.innerText;
-
-    var myTemporaryInputElement = document.createElement("input");
-    myTemporaryInputElement.type = "text";
-    myTemporaryInputElement.value = textToCopy;
-
-    document.body.appendChild(myTemporaryInputElement);
-
-    myTemporaryInputElement.select();
-    document.execCommand("Copy");
-
-    document.body.removeChild(myTemporaryInputElement);
-
-}
+                });
